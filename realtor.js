160419e6a6af922b590f68b6a29598892b4b820e
MJephat <mukundijephat22@gmail.com>
2023-04-05 13:31:20 +0300

    like button

--- realtor.js
+++ realtor.js
@@ -1,4 +1,4 @@
-console.log("hello")
+
 fetch("http://localhost:3000/houses",)
     .then(res => res.json())
     .then(data => {
@@ -6,14 +6,14 @@ fetch("http://localhost:3000/houses",)
         const  character = document.querySelector(".box")
 
         for(let i = 0; i < data.length; i++) {
-            console.log(data[i]);
+            // console.log(data[i]);
 
             let pic = data[i].image_url;
             let cost = data[i].price;
             let local = data[i].address;
             let  about = data[i].description;
 
-            console.log(data[i]);
+            // console.log(data[i]);
 
             const card = document.createElement("div");
             card.classList.add("card");
@@ -21,7 +21,11 @@ fetch("http://localhost:3000/houses",)
             <img src="${pic}"style="width:300px; height:250px;" />
             <h5>Price: ${cost}</h5>
             <h5>Address: ${local}</h5>
-            Description: ${about}
+            Description: ${about}</br>
+            <button class="like_btn">
+            <span id="icon"><i class="fa fa-heart"></i></span>
+            <span id="count">0</span> Like
+            </button>
 
             `
             character.appendChild(card)
@@ -31,7 +35,29 @@ fetch("http://localhost:3000/houses",)
     })
 
 
-    // filter function
+    // Like button function.
+    // grabbing our elements.
     
+    let likeButton = document.getElementsByClassName(".like_btn"); 
+    let likeIcon = document.getElementById("#icon");
+    let count = document.getElementById("#count");
+
+    // button clicked
+    let clicked = false;
+    
+    likeButton.addEventListener("click", tangle=>{
+        function tangle(){
+        if (!clicked){
+            clicked =true;
+            likeIcon.innerHTML = `<i class="fa fa-heart"></i>`;
+            count.textContent++;
+        }
+        else{
+            clicked = false;
+            likeIcon.innerHTML =`<i class="fa fa-heart"></i>`
+            count.textContent++;
+        }
+       }
+    });
 
 

6a12a22d74344acbb947651ea46f7a6bbf81eaa7
MJephat <mukundijephat22@gmail.com>
2023-04-07 18:12:05 +0300

    slide function writen

--- realtor.js
+++ realtor.js
@@ -110,3 +110,25 @@ form.addEventListener('submit',function(event){
     id.innerHTML = "Realtor properties!";
     }
 
+
+
+// slideshow
+
+ const productContainers = [...document.querySelectorAll(".product-container")];
+
+ const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+ const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+ productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener("click", () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener("click", () => {
+        item.scrollLeft -= containerWidth;
+    })
+ })
+
